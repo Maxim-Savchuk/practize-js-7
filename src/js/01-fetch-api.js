ca4a18d786485929f36cb9a7308148a5b0f565a0
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-21 23:05:06 +0300

    1

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -31,27 +31,27 @@ function onSearch(e) {
   API.fetchPokemon(searchQuery)
     .then(renderPokemonCard)
     .catch(onFetchError)
-    .finally(() => form.reset());
+    .finally(()=>{form.reset()});
 }
-
+  
 function renderPokemonCard(pokemon) {
   const markup = pokemonCardTpl(pokemon);
   refs.cardContainer.innerHTML = markup;
-}
-
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
-}
-
-// =========================================
-
-const url = 'https://newsapi.org/v2/everything?q=cars';
-const options = {
-  headers: {
-    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-  },
 };
 
-fetch(url, options)
-  .then(r => r.json())
-  .then(console.log);
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так и мы не нашли вашего покемона');
+}
+
+// // =========================================
+
+// const url = 'https://newsapi.org/v2/everything?q=cars';
+// const options = {
+//   headers: {
+//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+//   },
+// };
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
